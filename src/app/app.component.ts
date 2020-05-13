86559f282d447538a0cca658c05c0d72416cef86
geethapriya1795 <geet1795@gmail.com>
2020-05-13 14:38:14 +0530

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
-  name = 'Angular';
+export class AppComponent implements OnInit {
+  showSong:boolean;
+  songList;
+  albumList;
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit() {
+    this.showSong = true;
+    this.getSongList();
+    this.getAlbumList();
+  }
+
+  getSongList(){
+    this.http.get("https://jsonplaceholder.typicode.com/photos")
+    .subscribe((data) => this.songList = data);
+  }
+
+  getAlbumList(){
+    this.http.get("https://jsonplaceholder.typicode.com/albums")
+    .subscribe((data) => this.albumList = data);
+  }
+
+  getAlbum(albumId){
+    let album = this.albumList.find((album)=> album.userId == albumId)
+    return album.title;
+  }
+
 }
